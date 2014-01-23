2cf9ac6dfb4c505e7351733637d0924c5bdae0b5
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2014-01-23 18:15:05 +0900

    Add portrait mode.

--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,7 @@
 
 var React = require('react');
 
-var Contributions = React.createClass({
+var Color = {
   colors: [
     '#eeeeee',
     '#d6e685',
@@ -19,7 +19,13 @@ var Contributions = React.createClass({
     else if (count <= 9) return 3;
     else return 4;
   },
-  aggregateWeeks: function (days) {
+  forCount: function (count) {
+    return this.colors[this.colorIndex(count)];
+  }
+};
+
+var Contributions = React.createClass({
+  aggregateWeeks: function () {
     var firstDay = new Date(this.props.items[0][0]);
     var offset = firstDay.getDay();
     var weeks = [];
@@ -38,21 +44,15 @@ var Contributions = React.createClass({
     }
     return weeks;
   },
-  render: function () {
+  renderLandscape: function (weeks) {
     var self = this;
-
-    if (this.props.items.length === 0) {
-      return <p>No Data</p>;
-    }
-
-    var size = 15;
-    var margin = 4;
-    var weeks = self.aggregateWeeks();
+    var size = 4;
+    var margin = 1;
 
     var groups = weeks.map(function (week, i) {
       var rects = week.map(function (day, j) {
         if (day == null) return null;
-        var style = { fill: self.colors[self.colorIndex(day[1])] };
+        var style = { fill: Color.forCount(day[1]) };
         var y = j * (size + margin);
         return <rect width={size} height={size} y={y} x={0} style={style} key={day[0]} />;
       }).filter(Boolean);
@@ -61,8 +61,38 @@ var Contributions = React.createClass({
     });
 
     return (
-      <svg width="100%" height={500}>{groups}</svg>
+      <svg width={53 * (size + margin)} height={7 * (size + margin)}>{groups}</svg>
     );
+  },
+  renderPortrait: function (weeks) {
+    var self = this;
+    var size = 20;
+    var margin = 6;
+
+    var groups = weeks.reverse().map(function (week, i) {
+      var rects = week.reverse().map(function (day, j) {
+        if (day == null) return null;
+        var style = { fill: Color.forCount(day[1]) };
+        var x = j * (size + margin);
+        return <rect width={size} height={size} x={x} y={0} style={style} key={day[0]} />;
+      }).filter(Boolean);
+      var transform = 'translate(0,' + i * (size + margin) + ')';
+      return <g transform={transform} key={'week-' + i}>{rects}</g>;
+    });
+
+    return (
+      <svg width={7 * (size + margin)} height={53 * (size + margin)}>{groups}</svg>
+    );
+  },
+  render: function () {
+    if (this.props.items.length === 0) {
+      return <p>No Data</p>;
+    }
+
+    var weeks = this.aggregateWeeks();
+
+    if (this.props.isPortrait) return this.renderPortrait(weeks);
+    else return this.renderLandscape(weeks);
   }
 });
 
@@ -70,36 +100,43 @@ var Page = React.createClass({
   getInitialState: function () {
     return {
       contribs: [],
-      loading: false
+      isLoading: false,
+      isPortrait: window.orientation === 0
     };
   },
   componentDidMount: function () {
     var self = this;
 
+    // Get contributions calendar from Github.
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function () {
       if (xhr.readyState !== 4) return;
       if (xhr.status === 200) {
         self.setState({
-          loading: false,
+          isLoading: false,
           contribs: JSON.parse(xhr.responseText)
         });
       } else {
-        self.setState({ loading: false });
+        self.setState({ isLoading: false });
         console.log('Something went wrong.', xhr.status);
       }
     };
     xhr.open('GET', '/contributions/shuhei');
-    this.setState({ loading: true });
+    this.setState({ isLoading: true });
     xhr.send(null);
+
+    // Watch device orientation.
+    window.addEventListener('orientationchange', function () {
+      self.setState({ isPortrait: window.orientation === 0 });
+    });
   },
   render: function () {
     return (
       <div>
         <h1>{this.props.title}</h1>
-        {this.state.loading ?
+        {this.state.isLoading ?
           <p key="loading">Loading</p> :
-          <Contributions items={this.state.contribs} />
+          <Contributions items={this.state.contribs} isPortrait={this.state.isPortrait} />
         }
       </div>
     );
