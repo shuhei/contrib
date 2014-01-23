8d6f98c0542629a75399b6cb876b54c82b1e469c
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2014-01-23 15:43:34 +0900

    Show contributions.

--- src/js/app.js
+++ src/js/app.js
@@ -2,25 +2,59 @@
 
 'use strict';
 
+var Contributions = React.createClass({
+  render: function () {
+    console.log();
+    var items = this.props.contribs.map(function (date) {
+      return <li>{date[0]}: {date[1]}</li>;
+    });
+    return (
+      <ul>{items}</ul>
+    );
+  }
+});
+
 var Page = React.createClass({
+  getInitialState: function () {
+    return {
+      contribs: [],
+      loading: false
+    };
+  },
   componentDidMount: function () {
+    var self = this;
+
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function () {
-      console.log(arguments);
+      if (xhr.readyState !== 4) return;
+      if (xhr.status === 200) {
+        self.setState({
+          loading: false,
+          contribs: JSON.parse(xhr.responseText)
+        });
+      } else {
+        self.setState({ loading: false });
+        console.log('Something went wrong.', xhr.status);
+      }
     };
     xhr.open('GET', '/contributions/shuhei');
+    this.setState({ loading: true });
     xhr.send(null);
   },
   render: function () {
     return (
       <div>
         <h1>{this.props.title}</h1>
+        {this.state.loading ?
+          <p>Loading</p> :
+          <Contributions contribs={this.state.contribs} />
+        }
       </div>
     );
   }
 });
 
 React.renderComponent(
-  <Page title="Hello, World!" />,
+  <Page title="Github Contributions" />,
   document.getElementById('page')
 );
